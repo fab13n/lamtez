83c1007abdbed6ff8c35233bdfa80e7c4f4631c4
Fleutot <fleutot@gmail.com>
2017-08-30 12:07:58 +0200

    std extension now .ltz

--- check.sh
+++ check.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+SRC_EXT="ltz"
+DST_EXT="tz"
 SRC="$1"
-DST="$(basename $SRC .lt).tz"
+DST="$(basename $SRC .$SRC_EXT).$DST_EXT"
 make debug && \
-./main.d.byte -c $SRC -o $DST && \
+( rm $DST ; \
+  ./main.d.byte -c $SRC -o $DST ) && \
 echo -e '\n' && \
 cat $DST && \
 tezos-client typecheck program file:$DST
